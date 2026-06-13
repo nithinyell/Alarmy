df97e26340145843afe58a72816c3d4762832c49
Nithin Yell <nithinyell@gmail.com>
2026-06-13 11:41:14 -0400

    Initial Commit

---
+++ Alarmy/AlarmyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmyApp.swift
+//  Alarmy
+//
+//  Created by Nithin on 2026-06-13.
+//
+
+import SwiftUI
+
+@main
+struct AlarmyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmyTests/AlarmyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmyTests.swift
+//  AlarmyTests
+//
+//  Created by Nithin on 2026-06-13.
+//
+
+import Testing
+@testable import Alarmy
+
+struct AlarmyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
